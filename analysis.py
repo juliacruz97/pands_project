f17decaecf3705e2cd3b1697bc29dd030790a699
juliacruz97 <juliacruz97@hotmail.com>
2024-08-22 10:49:20 -0300

    22_08_2024T10:49

---
+++ analysis.py
@@ -0,0 +1,23 @@
+# Outputs a summary of each variable to a single text file,
+
+# Class of iris plant:
+
+# Iris Setosa:
+# sepal length -
+# sepal width -
+# petal length -
+# petal width -
+
+
+# Iris Versicolour:
+# sepal length -
+# sepal width -
+# petal length -
+# petal width -
+
+# Iris Virginica:
+# sepal length -
+# sepal width -
+# petal length -
+# petal width -
+
