9fd18b8d22c2273ee6f889513b02e8901099eb77
juliacruz97 <juliacruz97@hotmail.com>
2024-08-22 12:41:08 -0300

    22_08_2024T12:40

---
+++ analize.py
@@ -0,0 +1,30 @@
+ 
+ # Classification:  Iris Setosa 
+ 
+ # Iris setosa is a species of flowering plant in the Iridaceae family, 
+ # belonging to the Iris genus, which includes various species known for 
+ # their showy flowers. Iris setosa is commonly found in temperate regions 
+ # of the Northern Hemisphere, especially in wet and cold areas like 
+ # North America (particularly in Alaska) and parts of Europe and Asia.
+ 
+# Key Characteristics:
+ 
+# Height: Typically grows between 30 to 60 cm tall.
+
+# Leaves: Its leaves are long, narrow, and upright, with a grayish-green hue.
+
+# Flowers: The flowers are typical of the Iris genus, with three large, 
+# drooping outer petals (called "falls") and three smaller, upright inner petals 
+# (called "standards"). The flower color ranges from blue to purple, 
+# but there are also varieties with white or pink flowers.
+
+# Habitat: Iris setosa prefers wet environments, such as riverbanks, marshes, 
+# and meadows. It is well-adapted to cold climates, making it common in arctic 
+# and subarctic regions.
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+img = mpflower.imread('flower.png')
+imgplot = plt.imshow(flower)
+plt.show()
